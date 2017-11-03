e42e5dcbec4131182e226bfec927ba02e37cebd0
Khoa Pham <onmyway133@gmail.com>
2017-11-03 09:24:16 +0100

    Remove some configurators

--- SwiftProject/Sources/Config/CompassConfigurator.swift
+++
@@ -1,9 +0,0 @@
-import Compass
-import Sugar
-
-struct CompassConfigurator: Configurable {
-
-  func configure() {
-    Navigator.scheme = Application.executable.replacingOccurrences(of: " ", with: "-").lowercased()
-  }
-}
