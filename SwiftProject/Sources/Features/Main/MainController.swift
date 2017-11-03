9d74facaef9b9a51ceb55883343787250029dd30
Khoa Pham <onmyway133@gmail.com>
2017-11-03 09:37:08 +0100

    Remove imports of removed dependencies

--- SwiftProject/Sources/Features/Main/MainController.swift
+++ SwiftProject/Sources/Features/Main/MainController.swift
@@ -1,35 +1,5 @@
 import UIKit
-import Tabby
 
-class MainController: TabbyController {
+class MainController: UIViewController {
 
-  convenience init() {
-    self.init(items: [])
-  }
-
-  // MARK: - View lifecycle
-
-  override func viewDidLoad() {
-    super.viewDidLoad()
-
-    // Controllers
-    let firstController = UIViewController()
-    firstController.title = "First".uppercased()
-
-    let secondController = UIViewController()
-    secondController.title = "Second".uppercased()
-
-    // Tab bar
-    items = [
-      TabbyBarItem(
-        controller: UINavigationController(rootViewController: firstController),
-        image: ""),
-      TabbyBarItem(
-        controller: UINavigationController(rootViewController: secondController),
-        image: "")
-    ]
-
-    view.backgroundColor = UIColor.white
-    setIndex = 0
-  }
 }
