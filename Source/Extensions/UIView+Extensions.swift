b2894e7d74ca46cde93c7e3224eb98ce8878d2cc
Christoffer Winterkvist <christoffer@winterkvist.com>
2018-04-25 20:21:43 +0200

    Remove addParallaxMotionEffects

--- Source/Extensions/UIView+Extensions.swift
+++ Source/Extensions/UIView+Extensions.swift
@@ -6,22 +6,4 @@ extension UIView {
       addSubview(view)
     }
   }
-
-  func addParallaxMotionEffects(tiltValue: CGFloat = 0.25, panValue: CGFloat = 5, to view: UIView?) {
-    let motionGroup = UIMotionEffectGroup()
-    let xTilt = UIInterpolatingMotionEffect(keyPath: "layer.transform.rotation.y", type: .tiltAlongHorizontalAxis)
-    xTilt.minimumRelativeValue = -tiltValue
-    xTilt.maximumRelativeValue = tiltValue
-    let yTilt = UIInterpolatingMotionEffect(keyPath: "layer.transform.rotation.x", type: .tiltAlongVerticalAxis)
-    yTilt.minimumRelativeValue = tiltValue
-    yTilt.maximumRelativeValue = -tiltValue
-    let xPan = UIInterpolatingMotionEffect(keyPath: "center.x", type: .tiltAlongHorizontalAxis)
-    xPan.minimumRelativeValue = -panValue
-    xPan.maximumRelativeValue = panValue
-    let yPan = UIInterpolatingMotionEffect(keyPath: "center.y", type: .tiltAlongVerticalAxis)
-    yPan.minimumRelativeValue = -panValue
-    yPan.maximumRelativeValue = panValue
-    motionGroup.motionEffects = [xTilt, yTilt, xPan, yPan]
-    view?.addMotionEffect(motionGroup)
-  }
 }
